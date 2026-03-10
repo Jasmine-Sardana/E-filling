1eeebdeadcfcabab8a04097a311f3ddbde66d24f
Jasmine <jasminesardana@gmail.com>
2026-03-10 15:35:25 +0530

    Choose us page

--- src/layout/ChooseUs.jsx
+++ src/layout/ChooseUs.jsx
@@ -1,9 +1,63 @@
 import React from 'react'
+import { Award, UserCheck, Zap, Shield } from 'lucide-react'
 
-export const ChooseUs = () => {
-  return (
-    <div>ChooseUs</div>
-  )
+
+const features = [
+    {
+        icon : Award,
+        title: "20+ Years of Experience",
+        description: "Proven track record in delivering exceptional financial services"
+    },
+    {
+        icon: UserCheck,
+        title: 'Expert CA Professionals',
+        description: 'Highly qualified and certified chartered accountants',
+    },
+    {
+        icon: Zap,
+        title: 'Fast & Reliable Service',
+        description: 'Quick turnaround time without compromising on quality',
+    },
+    {
+    icon: Shield,
+        title: 'Trusted by Businesses',
+        description: 'Serving hundreds of satisfied clients across industries',
+    },
+];
+
+export function ChooseUs(){
+    return(
+        <section className='bg-gray-300 lg:px-8 sm:px-6'>
+            <div className=' max-w-4xl mx-auto text-center'>
+                <h2 className='text-3xl font-semibold lg:p-5 '> Why Choose Us?</h2>
+            </div>
+            
+            <div className='grid lg: grid-cols-2 gap-10 p-4 '>
+                {features.map((features,index) =>{
+                    const Icon = features.icon;
+
+                    return(
+                        <div
+                            key = {index}
+                            className='text-center group'>
+                            <div className='w-20 h-20 group-hover:scale-110 transition-transform bg-red-600 rounded-2xl flex items-center justify-center mx-auto '>
+                            <Icon className="text-white mb-4 size={36} "/>
+                            </div>
+                            <h3 className="text-xl font-semibold mb-2">
+                                {features.title}
+                            </h3>
+                            <p className="text-gray-600">
+                                {features.description}
+                            </p>
+
+                        </div>
+                    )
+
+                })}
+            </div>
+
+
+        </section>
+    )
 }
 
-export default ChooseUs
